a3937cd746ac49513237a20d80a3dc49c04e49dd
sharris1105 <sharris@practicevelocity.com>
2017-06-19 13:16:18 -0500

    Add files via upload

---
+++ Rules.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using DevExpress.Xpf.Core;
+
+
+namespace Yahtzee
+{
+    /// <summary>
+    /// Interaction logic for Rules.xaml
+    /// </summary>
+    public partial class Rules : DXWindow
+    {
+        public Rules()
+        {
+            InitializeComponent();
+        }
+
+        public string MyText()
+        {
+            return "My Text \n Your Text";
+        }
+    }
+}
